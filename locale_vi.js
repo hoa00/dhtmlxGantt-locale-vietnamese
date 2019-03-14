537a88d52ef67e9dfade242ce824c5c504385f64
Hòa <m@hoadev.com>
2019-03-14 16:57:28 +0700

    done translate

--- locale_vi.js
+++ locale_vi.js
@@ -115,68 +115,68 @@ return /******/ (function(modules) { // webpackBootstrap
 module.exports = function (gantt) {
 	gantt.locale = {
 		date: {
-			month_full: ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"],
-			month_short: ["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"],
-			day_full: ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"],
-			day_short: ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"]
+			month_full: ["Tháng một", "Tháng hai", "Tháng ba", "Tháng tư", "Tháng năm", "Tháng sáu", "Tháng bảy", "Tháng tám", "Tháng chín", "Tháng mười", "Tháng mười một", "Tháng mười hai"],
+			month_short: ["Tháng 1", "Tháng 2", "Tháng 3", "Tháng 4", "Tháng 5", "Tháng 6", "Tháng 7", "Tháng 8", "Tháng 9", "Tháng 10", "Tháng 11", "Tháng 12"],
+			day_full: ["Chủ Nhật", "Thứ hai", "Thứ ba", "Thứ tư", "Thứ năm", "Thứ sáu", "Thứ bảy"],
+			day_short: ["CN", "Thứ 2", "Thứ 3", "Thứ 4", "Thứ 5", "Thứ 6", "Thứ 7"]
 		},
 		labels: {
-			new_task: "New task",
-			icon_save: "Save",
-			icon_cancel: "Cancel",
-			icon_details: "Details",
-			icon_edit: "Edit",
-			icon_delete: "Delete",
-			confirm_closing: "",//Your changes will be lost, are you sure?
-			confirm_deleting: "Task will be deleted permanently, are you sure?",
-			section_description: "Description",
-			section_time: "Time period",
-			section_type: "Type",
+			new_task: "Việc mới",
+			icon_save: "Lưu",
+			icon_cancel: "Hủy",
+			icon_details: "Chi tiết",
+			icon_edit: "Sửa",
+			icon_delete: "Xóa",
+			confirm_closing: "Thay đổi của bạn sẽ không được lưu lại, bạn có chắc chắn?",//Your changes will be lost, are you sure?
+			confirm_deleting: "Việc này sẽ bị xóa, bạn có chắc chắn?",
+			section_description: "Mô tả",
+			section_time: "Khoảng thời gian",
+			section_type: "Kiểu",
 
 			/* grid columns */
 			column_wbs: "WBS",
-			column_text: "Task name",
-			column_start_date: "Start time",
-			column_duration: "Duration",
+			column_text: "Tên việc",
+			column_start_date: "Thời gian bắt đầu",
+			column_duration: "Thời lượng",
 			column_add: "",
 
 			/* link confirmation */
-			link: "Link",
-			confirm_link_deleting: "will be deleted",
-			link_start: " (start)",
-			link_end: " (end)",
+			link: "Liên kết",
+			confirm_link_deleting: "sẽ bị xóa",
+			link_start: " (bắt đầu)",
+			link_end: " (kết thúc)",
 
-			type_task: "Task",
-			type_project: "Project",
-			type_milestone: "Milestone",
+			type_task: "Việc",
+			type_project: "Dự án",
+			type_milestone: "Cột mốc",
 
-			minutes: "Minutes",
-			hours: "Hours",
-			days: "Days",
-			weeks: "Week",
-			months: "Months",
-			years: "Years",
+			minutes: "Phút",
+			hours: "Giờ",
+			days: "Ngày",
+			weeks: "Tuần",
+			months: "Tháng",
+			years: "Năm",
 
 			/* message popup */
-			message_ok: "OK",
-			message_cancel: "Cancel",
+			message_ok: "Đồng ý",
+			message_cancel: "Hủy",
 
 			/* constraints */
-			section_constraint: "Constraint",
-			constraint_type: "Constraint type",
-			constraint_date: "Constraint date",
-			asap: "As Soon As Possible",
-			alap: "As Late As Possible",
-			snet: "Start No Earlier Than",
-			snlt: "Start No Later Than",
-			fnet: "Finish No Earlier Than",
-			fnlt: "Finish No Later Than",
-			mso: "Must Start On",
-			mfo: "Must Finish On",
+			section_constraint: "Ràng buộc",
+			constraint_type: "Loại ràng buộc",
+			constraint_date: "Ngày ràng buộc",
+			asap: "Sớm nhất có thể",
+			alap: "Muộn nhất có thể",
+			snet: "Bắt đầu không sớm hơn",
+			snlt: "Bắt đầu không muộn hơn",
+			fnet: "Kết thúc không sớm hơn",
+			fnlt: "Kết thúc không muộn hơn",
+			mso: "Phải bắt đầu lúc",
+			mfo: "Phải kết thúc lúc",
 
 			/* resource control */
-			resources_filter_placeholder: "type to filter",
-			resources_filter_label: "hide empty"
+			resources_filter_placeholder: "kiểu đẽ lọc",
+			resources_filter_label: "ẩn cột trống"
 		}
 	};
 };
